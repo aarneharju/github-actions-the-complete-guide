52411058c693ff9abcff9799c9a53ae43b49b2db
Aarne Harju <aarne.harju@sogeti.com>
2024-03-22 10:55:41 +0200

    Added custom depoloy to aws action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,7 +4,19 @@ const exec = require('@actions/exec');
 
 
 function run() {
-    core.notice('Hello from my custom JavaScript Actiond')
+    // Get input values
+    const bucket = core.getInput("aws-bucket", { required: true });
+    const bucketRegion = core.getInput("aws-bucket-region", { required: true});
+    const distFolder = core.getInput("dist-folder", { required: true});
+
+    // Upload files
+    const s3URI = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`) // uses AWS CLI that is installed on the runner machine
+
+    // github.getOctokit() could be used to send requests to the github rest api
+    // github.context provides access to some values of the github context object
+
+    core.notice('Hello from my custom JavaScript Action')
 }
 
 run();
